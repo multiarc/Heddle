f6a7046240fa697eeb9bed8ea4578152b6bd1cb6
Aliaksandr Kukrash <multiarc@gmail.com>
2014-10-10 01:56:19 +0300

    VS 2012 config revert, remove some build targets, .NET 4.5.2 migration TEST

--- TTL VS Extension/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TTL {
-    public static class Configuration {
-        public const string ContentType = "text/ttl";
-        public const string FileExtension = ".ttl";
-        public const string EditorExtensionName = "ttl";
-        public const string HtmlFileExtension = ".httl";
-    }
-}
