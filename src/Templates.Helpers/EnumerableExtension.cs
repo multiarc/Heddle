51c3ddd267f1893a1d929b7d76aa0add2162a6f3
Aliaksandr Kukrash <multiarc@gmail.com>
2015-01-27 04:00:24 +0300

    By Design: If you use .ToSmartList<T>() with arrays then nothing is copied and if you change the items this will affect destination collection.

--- src/Templates.Helpers/EnumerableExtension.cs
+++ src/Templates.Helpers/EnumerableExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Templates.Strings.Core;
 
 namespace Templates.Helpers
@@ -6,6 +7,11 @@ namespace Templates.Helpers
     public static class EnumerableExtension {
         public static SmartList<T> ToSmartArray<T> (this IEnumerable<T> values)
         {
+            var arr = values as T[];
+            if (arr != null)
+            {
+                return new SmartList<T>(arr);
+            }
             var result = new SmartList<T>();
             if (values != null) {
                 foreach (T value in values)
