2662ce9272d68104e6b65f2b470fa460ae37b9f9
Aliaksandr Kukrash <multiarc@gmail.com>
2015-03-31 00:45:07 +0300

    Manual push to nuget

--- build.ps1
+++ build.ps1
@@ -11,3 +11,6 @@ Push-Location Templates
 dnu pack --out ..\..\pack --configuration Release
 Pop-Location
 Pop-Location
+Push-Location .nuget
+nuget push ..\pack\Release\Templates.Language.1.0.0.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
+nuget push ..\pack\Release\Templates.2.0.0.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
