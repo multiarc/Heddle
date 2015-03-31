209af5664a0979bbb4b3f376feb5545458b2e673
Aliaksandr Kukrash <multiarc@gmail.com>
2015-03-31 10:29:13 +0300

    Try teamcity build

--- build.ps1
+++ build.ps1
@@ -5,13 +5,9 @@ dnu build --configuration Release
 dnx . test
 Pop-Location
 Push-Location Templates.Language
-dnu pack --out ..\..\pack --configuration Release
+dnu pack --out ..\..\packages --configuration Release
 Pop-Location
 Push-Location Templates
-dnu pack --out ..\..\pack --configuration Release
+dnu pack --out ..\..\packages --configuration Release
 Pop-Location
-Pop-Location
-Push-Location .nuget
-.\nuget.exe push ..\pack\Release\Templates.Language.1.0.0.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
-.\nuget.exe push ..\pack\Release\Templates.2.0.0.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
 Pop-Location
