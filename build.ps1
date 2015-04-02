70440372513f8907a042f42c8cd17ad2537ca767
Aliaksandr Kukrash <multiarc@gmail.com>
2015-04-02 11:05:56 +0300

    nuget won't accept 1.0.0-1

--- build.ps1
+++ build.ps1
@@ -11,5 +11,5 @@ Push-Location Templates
 dnu pack --out ..\..\packages --configuration Release
 Pop-Location
 Pop-Location
-.\tools\nuget\nuget.exe push packages\Release\Templates.Language.1.0.0-1.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
-.\tools\nuget\nuget.exe push packages\Release\Templates.2.0.0-1.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
+.\tools\nuget\nuget.exe push packages\Release\Templates.Language.1.0.1.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
+.\tools\nuget\nuget.exe push packages\Release\Templates.2.0.1.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
