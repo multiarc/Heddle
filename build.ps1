f856a6528a4059ac930dc266c923a5ad54b80844
Aliaksandr Kukrash <multiarc@gmail.com>
2015-05-04 01:20:12 +0200

    VS RC Support

--- build.ps1
+++ build.ps1
@@ -12,4 +12,4 @@ dnu pack --out ..\..\packages --configuration Release
 Pop-Location
 Pop-Location
 .\tools\nuget\nuget.exe push packages\Release\Templates.Language.1.1.1.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
-.\tools\nuget\nuget.exe push packages\Release\Templates.2.2.1.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
+.\tools\nuget\nuget.exe push packages\Release\Templates.2.2.2.nupkg 351e721d-773c-4de3-9583-119c28829995 -Source https://www.myget.org/F/antlrcs/
