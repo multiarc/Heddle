4fdf3bf0310bdd31cd6238778cada73e09252771
Aliaksandr Kukrash <multiarc@gmail.com>
2021-03-05 17:09:43 +0300

    Add JS linter

--- src/Templates.Language/js/ace/lib/ace/mode/ttl_worker.js
+++ src/Templates.Language/js/ace/lib/ace/mode/ttl_worker.js
@@ -8,12 +8,44 @@ define(function (require, exports, module) {
     var DocumentParser = require("./ttl/DocumentParser").DocumentParser;
     var SAXParser = require("./html/saxparser").SAXParser;
     var CSSLint = require("./css/csslint").CSSLint;
+    var lint = require("./javascript/jshint").JSHINT;
 
     var htmlErrorTypes = {
         "expected-doctype-but-got-start-tag": "info",
         "expected-doctype-but-got-chars": "info",
         "non-html-root": "info"
     };
+
+    function startRegex(arr) {
+        return RegExp("^(" + arr.join("|") + ")");
+    }
+
+    var disabledWarningsRe = startRegex([
+        "Bad for in variable '(.+)'.",
+        'Missing "use strict"'
+    ]);
+    var errorsRe = startRegex([
+        "Unexpected",
+        "Expected ",
+        "Confusing (plus|minus)",
+        "\\{a\\} unterminated regular expression",
+        "Unclosed ",
+        "Unmatched ",
+        "Unbegun comment",
+        "Bad invocation",
+        "Missing space after",
+        "Missing operator at"
+    ]);
+    var infoRe = startRegex([
+        "Expected an assignment",
+        "Bad escapement of EOL",
+        "Unexpected comma",
+        "Unexpected space",
+        "Missing radix parameter.",
+        "A leading decimal point can",
+        "\\['{a}'\\] is better written in dot notation.",
+        "'{a}' used out of scope"
+    ]);
 
     var TtlWorker = exports.TtlWorker = function(sender) {
         Mirror.call(this, sender);
@@ -24,6 +56,7 @@ define(function (require, exports, module) {
             "adjoining-classes|qualified-headings|zero-units|gradients|" +
             "import|outline-none|vendor-prefix"
         );
+        this.setOptions();
     };
 
     oop.inherits(TtlWorker, Mirror);
@@ -44,7 +77,26 @@ define(function (require, exports, module) {
         }
         
         this.setOptions = function(options) {
-            this.options = options || {};
+            this.options = options || {
+                jsOptions: {
+                    // undef: true,
+                    // unused: true,
+                    esnext: true,
+                    moz: true,
+                    devel: true,
+                    browser: true,
+                    node: true,
+                    laxcomma: true,
+                    laxbreak: true,
+                    lastsemic: true,
+                    onevar: false,
+                    passfail: false,
+                    maxerr: 100,
+                    expr: true,
+                    multistr: true,
+                    globalstrict: true
+                }
+            };
             this.doc.getValue() && this.deferredUpdate.schedule(100);
         };
 
@@ -79,6 +131,85 @@ define(function (require, exports, module) {
             oop.mixin(this.options, newOptions);
             this.doc.getValue() && this.deferredUpdate.schedule(100);
         };
+
+        this.isValidJS = function(str) {
+            try {
+                // evaluated code can only create variables in this function
+                eval("throw 0;" + str);
+            } catch(e) {
+                if (e === 0)
+                    return true;
+            }
+            return false;
+        };
+        
+        this.validateJs = function(value) {
+            if (!value)
+                return [];
+            
+            value = value.replace(/^#!.*\n/, "\n");
+            if (!value)
+                return [];
+
+            var errors = [];
+            // jshint reports many false errors
+            // report them as error only if code is actually invalid
+            var maxErrorLevel = this.isValidJS(value) ? "warning" : "error";
+
+            // var start = new Date();
+            lint(value, this.options.jsOptions, this.options.jsOptions.globals);
+            var results = lint.errors;
+
+            var errorAdded = false;
+            for (var i = 0; i < results.length; i++) {
+                var error = results[i];
+                if (!error)
+                    continue;
+                var raw = error.raw;
+                var type = "warning";
+
+                if (raw == "Missing semicolon.") {
+                    var str = error.evidence.substr(error.character);
+                    str = str.charAt(str.search(/\S/));
+                    if (maxErrorLevel == "error" && str && /[\w\d{(['"]/.test(str)) {
+                        error.reason = 'Missing ";" before statement';
+                        type = "error";
+                    } else {
+                        type = "info";
+                    }
+                }
+                else if (disabledWarningsRe.test(raw)) {
+                    continue;
+                }
+                else if (infoRe.test(raw)) {
+                    type = "info";
+                }
+                else if (errorsRe.test(raw)) {
+                    errorAdded  = true;
+                    type = maxErrorLevel;
+                }
+                else if (raw == "'{a}' is not defined.") {
+                    type = "warning";
+                }
+                else if (raw == "'{a}' is defined but never used.") {
+                    type = "info";
+                }
+
+                errors.push({
+                    row: error.line-1,
+                    column: error.character-1,
+                    text: error.reason,
+                    type: type,
+                    raw: raw
+                });
+
+                if (errorAdded) {
+                    // break;
+                }
+            }
+            
+            return errors;
+        }
 
         this.onUpdate = function() {
             var value = this.doc.getValue();
@@ -161,7 +292,6 @@ define(function (require, exports, module) {
                 
                 if (htmlString) {
                     var saxParser = new SAXParser();
-                    var htmlErrors = [];
                     var noop = function () {
                     };
                     saxParser.contentHandler = {
@@ -173,7 +303,7 @@ define(function (require, exports, module) {
                     };
                     saxParser.errorHandler = {
                         error: function (message, location, code) {
-                            htmlErrors.push({
+                            errors.push({
                                 row: location.line,
                                 column: location.column,
                                 text: message,
@@ -182,25 +312,31 @@ define(function (require, exports, module) {
                         }
                     };
                     saxParser.parse(htmlString);
-                    this.sender.emit("error", htmlErrors);
                 }
-                
                 var cssString = processEmbeddedLanguageLines(cssTokens);
                 
                 if (cssString) {
                     var infoRules = this.infoRules;
 
                     var result = CSSLint.verify(cssString, this.ruleset);
-                    this.sender.emit("annotate", result.messages.map(function(msg) {
-                        return {
+
+                    result.messages.forEach(function (msg) {
+                        errors.push({
                             row: msg.line - 1,
                             column: msg.col - 1,
                             text: msg.message,
                             type: infoRules[msg.rule.id] ? "info" : msg.type,
                             rule: msg.rule.name
-                        };
-                    }));
+                        });
+                    });
                 }
+                
+                var jsString = processEmbeddedLanguageLines(jsTokens);
+                jsString = jsString.replace(/^#!.*\n/, "\n");
+                
+                errors.push(this.validateJs(jsString));
+                
+                this.sender.emit("annotate", errors);
             }
         };
 
