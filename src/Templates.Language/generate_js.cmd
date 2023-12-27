c169d39095f479c3c0ac9484bab572ff4ceeb7c9
Aliaksandr Kukrash <multiarc@gmail.com>
2023-12-27 15:03:29 +0400

    Package updates

--- src/Templates.Language/generate_js.cmd
+++ src/Templates.Language/generate_js.cmd
@@ -1,1 +1,1 @@
-java -jar "..\..\lib\antlr-4.9.3-complete.jar" -Dlanguage=JavaScript "TtlLexer.g4" "TtlParser.g4" -o "js" -lib "js" -package Templates.Language
+java -jar "..\..\lib\antlr-4.13.1-complete.jar" -Dlanguage=JavaScript "TtlLexer.g4" "TtlParser.g4" -o "js" -lib "js" -package Templates.Language
